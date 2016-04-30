c3ff5ab5d11f452447e272119f4525e0c760884f
TruemenHale <truemenhale@gmail.com>
2016-05-01 02:55:26 +0800

    over

--- js/main.js
+++ js/main.js
@@ -2,6 +2,21 @@
  * Created by truemenhale on 16/5/1.
  */
 $(function(){
+    $.mobile.loading('show');
+    var aImg = ['MainPage.jpg','LeadPage.jpg','QRcode.jpg','tuanhui.png'];
+    var aLoaded = [];
+    var n = 0;
+    for(var i = 0,len = aImg.length; i < len; i++){
+        var img = new Image();
+        img.src = 'images/' + aImg[i];
+        img.onload = function(){
+            n+=1;
+            if(n == 4){
+                $.mobile.loading('hide');
+                $.mobile.changePage('#LeadPage');
+            }
+        }
+    }
    $('#LeadPage').on('scroll',function(){
        $.mobile.changePage('#MainPage',{
            transition: 'slideup'
