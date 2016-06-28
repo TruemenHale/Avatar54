dab60a8591d24c0bbdf7bb113421acd7167d275f
T&H <truemenhale@gmail.com>
2016-06-28 14:38:24 +0800

    g

--- js/main.js
+++ js/main.js
@@ -23,16 +23,34 @@ function toMain(){
 $(function(){
     $.mobile.loading('show');
     var aImg = ['MainPage.jpg','LeadPage.jpg','QRcode.jpg','tuanhui.png'];
-    var aLoaded = [];
+    var oT;
     var n = 0;
+    var w = $(window).width()*0.74;
+    var canvas = $('.cav');
+    canvas.attr('width',w);
+    canvas.attr('height',w);
+    var gd = canvas[0].getContext('2d');
     for(var i = 0,len = aImg.length; i < len; i++){
         var img = new Image();
         img.src = 'images/' + aImg[i];
+        if(aImg[i] == 'tuanhui.png'){
+            oT = img;
+        }
         img.onload = function(){
             n+=1;
             if(n == 4){
-                $.mobile.loading('hide');
-                $.mobile.changePage('#LeadPage');
+                var avatar = new Image();
+                avatar.src = 'images/avatar.png';
+                avatar.onload = function(){
+                    console.log(avatar.width+','+avatar.height);
+                    gd.save();
+                    gd.drawImage(avatar,1,w*0.11,w*0.87,w*0.87);
+                    gd.drawImage(oT,w*0.7,0,w*0.3,w*0.3);
+                    gd.restore();
+                    $.mobile.loading('hide');
+                    $.mobile.changePage('#LeadPage');
+                };
+
             }
         }
     }
